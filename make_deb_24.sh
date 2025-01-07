a0e08591bceb91682ef0acdc70e077faa2c1ad07
chamalsl <1069808+chamalsl@users.noreply.github.com>
2025-01-07 08:49:08 +0530

    Introduced SHASUMS file. This file contains sha256sum for build files

--- make_deb_24.sh
+++ make_deb_24.sh
@@ -1,9 +1,11 @@
 VERSION=0.1
 PATCH_VERSION=1
-RELEASE_DIR=release/Ubuntu24/is-online_${VERSION}-${PATCH_VERSION}_amd64
+RELEASE_FILE=is-online_${VERSION}-${PATCH_VERSION}_amd64
+RELEASE_DIR=release/Ubuntu24/${RELEASE_FILE}
 mkdir -p ${RELEASE_DIR}/usr/local/bin
 mkdir -p ${RELEASE_DIR}/DEBIAN
 touch ${RELEASE_DIR}/DEBIAN/control
+cp build/release/is-online release/Ubuntu24/
 cp build/release/is-online ${RELEASE_DIR}/usr/local/bin/is-online
 
 echo "Package: is-online" > ${RELEASE_DIR}/DEBIAN/control
@@ -13,6 +15,10 @@ echo "Maintainer: Chamal De Silva <chamaldesilva@gmail.com>" >> ${RELEASE_DIR}/D
 echo "Description: Program which checks whether a web site is online." >> ${RELEASE_DIR}/DEBIAN/control
 echo "Depends: libgtkmm-3.0-1t64 (>= 3.24.9), libcurl4t64 (>= 7.16.2) , libsqlite3-0 (>= 3.5.9)" >> ${RELEASE_DIR}/DEBIAN/control
 cd release/Ubuntu24/
-dpkg-deb --build --root-owner-group is-online_0.1-1_amd64
+dpkg-deb --build --root-owner-group ${RELEASE_FILE}/
+
+#Generate SHASUMS file
+sha256sum is-online >> SHASUMS
+sha256sum ${RELEASE_FILE}.deb >> SHASUMS
 
 
